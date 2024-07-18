b8ee158b3b21d7e8e9092dd93dd73d12a5eb59c4
Bruno Martins <bgcngm@gmail.com>
2024-07-18 17:50:29 +0530

    sm8475-common: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd

--- sm8475.mk
+++ sm8475.mk
@@ -273,10 +273,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # RFS MSM MPSS symlinks
 PRODUCT_PACKAGES += \
@@ -290,7 +288,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/motorola
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/motorola \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
