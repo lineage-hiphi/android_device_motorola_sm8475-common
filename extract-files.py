d82c59fba8914b358cb92290dfbc5a5f47869a10
Bruno Martins <bgcngm@gmail.com>
2024-10-01 22:35:45 +0100

    sm8475-common: Address seccomp filter failures
    Fixes:
     W ATFWD-daemon: libminijail[4334]: logging seccomp filter failures
     E ATFWD-daemon: libminijail[4334]: blocked syscall: gettid
     I init    : Service 'vendor.atfwd' (pid 4334) received signal 31
     I init    : Sending signal 9 to service 'vendor.atfwd' (pid 4334) process group...
     ...
     W wfdhdcphalservice: libminijail[5312]: logging seccomp filter failures
     E wfdhdcphalservice: libminijail[5312]: blocked syscall: gettid
     I init    : Service 'wfdhdcphalservice' (pid 5312) received signal 31
     I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 5312) process group...
    
    Change-Id: Ic259464520db6e77f06a3d0820a63662aa91659c

--- extract-files.py
+++ extract-files.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
 # SPDX-FileCopyrightText: The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
@@ -9,11 +8,11 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
 from extract_utils.main import (
-    ExtractUtils,
     ExtractUtilsModule,
 )
 
@@ -22,46 +21,44 @@ namespace_imports = [
     'hardware/motorola',
     'hardware/qcom-caf/sm8450',
     'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
 ]
 
 
+libs_add_vendor_suffix = (
+    'vendor.qti.hardware.qccsyshal@1.0',
+    'vendor.qti.hardware.qccsyshal@1.1',
+    'vendor.qti.imsrtpservice@3.0',
+    'vendor.qti.diaghal@1.0',
+    'vendor.qti.hardware.wifidisplaysession@1.0',
+    'com.qualcomm.qti.dpm.api@1.0',
+)
+
+
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'vendor' else None
+    if partition != 'vendor':
+        return None
+
+    return f'{lib}_{partition}'
 
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    (
-        'com.qualcomm.qti.dpm.api@1.0',
-        'vendor.qti.diaghal@1.0',
-        'vendor.qti.hardware.dpmservice@1.0',
-        'vendor.qti.hardware.dpmservice@1.1',
-        'vendor.qti.hardware.qccsyshal@1.0',
-        'vendor.qti.hardware.qccsyshal@1.1',
-        'vendor.qti.hardware.qccvndhal@1.0',
-        'vendor.qti.imsrtpservice@3.0',
-    ): lib_fixup_vendor_suffix,
+    libs_add_vendor_suffix: lib_fixup_vendor_suffix,
 }
+
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/permissions/moto-telephony.xml': blob_fixup().regex_replace(
         '/system/', '/system_ext/'
     ),
-    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
-    (
-        'vendor/lib64/libgarden.so',
-        'vendor/lib64/libgarden_haltests_e2e.so'
-    ): blob_fixup()
-    .replace_needed(
-        'android.hardware.gnss-V1-ndk_platform.so',
-        'android.hardware.gnss-V1-ndk.so'
-    )
-    .replace_needed(
-        'vendor.qti.gnss-V3-ndk_platform.so',
-        'vendor.qti.gnss-V5-ndk_platform.so'
+    'system_ext/lib64/libwfdnative.so': blob_fixup().add_needed(
+        'libinput_shim.so'
+    ),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup().replace_needed(
+        'libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'
     ),
     (
         'vendor/bin/hw/android.hardware.security.keymint-service-qti',
@@ -80,42 +77,43 @@ blob_fixups: blob_fixups_user_type = {
         'android.hardware.security.sharedsecret-V1-ndk.so',
     )
     .add_needed('android.hardware.security.rkp-V1-ndk.so'),
-    'vendor/bin/init.kernel.post_boot.sh': blob_fixup()
-        .regex_replace('ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'),
-    'vendor/bin/qcc-trd': blob_fixup().replace_needed(
-        'libgrpc++_unsecure.so', 'libgrpc++_unsecure_prebuilt.so'
-    ),
-    'system_ext/priv-app/ims/ims.apk': blob_fixup().apktool_patch(
-        'ims-patches'
-    ),
-    'vendor/lib64/vendor.libdpmframework.so': blob_fixup().add_needed(
-        'libhidlbase_shim.so'
-    ),
-    'vendor/lib64/sensors.moto.so': blob_fixup().add_needed('libbase_shim.so'),
-    'vendor/etc/media_codecs_cape.xml': blob_fixup().regex_replace(
-        '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).*\n', ''
-    ),
-    (
-        'vendor/bin/poweropt-service',
-        'vendor/lib64/libapengine.so',
-        'vendor/lib64/libdpps.so',
-        'vendor/lib64/libgamepoweroptfeature.so',
-        'vendor/lib64/liblearningmodule.so',
-        'vendor/lib64/libpowercore.so',
-        'vendor/lib64/libpsmoptfeature.so',
-        'vendor/lib64/libsnapdragoncolor-manager.so',
-        'vendor/lib64/libstandbyfeature.so',
-        'vendor/lib64/libvendorscenariopoweroptfeature.so',
-        'vendor/lib64/libvideooptfeature.so',
-    ): blob_fixup()
-    .replace_needed(
-        'libtinyxml2.so',
-        'libtinyxml2-v34.so',
+    'vendor/bin/init.kernel.post_boot.sh': blob_fixup().regex_replace(
+        'ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'
     ),
     'vendor/etc/sensors/hals.conf': blob_fixup().add_line_if_missing(
         'sensors.moto_ext.so',
     ),
-}  # fmt: skip
+    'vendor/lib64/libmotext_inf.so': blob_fixup().remove_needed('libril.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup().add_needed(
+        'libhidlbase_shim.so'
+    ),
+    'vendor/lib64/libqcodec2_core.so': blob_fixup().add_needed(
+        'libcodec2_shim.so'
+    ),
+    'vendor/lib64/sensors.moto.so': blob_fixup().add_needed('libbase_shim.so'),
+    (
+        'vendor/etc/media_codecs_waipio_v0.xml',
+        'vendor/etc/media_codecs_waipio_v1.xml',
+        'vendor/etc/media_codecs_waipio_v2.xml',
+        'vendor/etc/media_codecs_ravelin.xml',
+    ): blob_fixup().regex_replace(
+        '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).*\n',
+        '',
+    ),
+    (
+        'vendor/etc/seccomp_policy/atfwd@2.0.policy',
+        'vendor/etc/seccomp_policy/modemManager.policy',
+        'vendor/etc/seccomp_policy/wfdhdcphalservice.policy',
+    ): blob_fixup().add_line_if_missing(
+        'gettid: 1'
+    ),
+    (
+        'vendor/lib64/libdpps.so',
+        'vendor/lib64/libsnapdragoncolor-manager.so',
+    ): blob_fixup().replace_needed(
+        'libtinyxml2.so', 'libtinyxml2-v34.so'
+    ),
+}
 
 module = ExtractUtilsModule(
     'sm8475-common',
@@ -124,7 +122,3 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
-
-if __name__ == '__main__':
-    utils = ExtractUtils.device(module)
-    utils.run()
